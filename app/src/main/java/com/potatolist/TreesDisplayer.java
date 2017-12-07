46b56f4e80ec8176d6747e3aba1097c96e7b4d15
CNC3P0 <rspanell@hotmail.com>
2017-12-06 20:55:03 -0800

    Finished.

--- app/src/main/java/com/potatolist/TreesDisplayer.java
+++ app/src/main/java/com/potatolist/TreesDisplayer.java
@@ -13,11 +13,17 @@ public class TreesDisplayer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trees_displayer);
         TextView textView = findViewById(R.id.tv);
+        TextView textView1 = findViewById(R.id.tv1);
+        TextView textView2 = findViewById(R.id.tv2);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        String tree = bundle.get("COMMON_NAME") + "\n\n" + bundle.get("SCIENTIFIC_NAME") + "\n\n" + bundle.get("DESCRIPTION");
+        String tree = (String) bundle.get("COMMON_NAME");
+        String tree1 = (String) bundle.get("SCIENTIFIC_NAME");
+        String tree2 = (String) bundle.get("DESCRIPTION");
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(bundle.getInt("IMAGE"));
         textView.setText(tree);
+        textView1.setText(tree1);
+        textView2.setText(tree2);
     }
 }
